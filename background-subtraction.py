fixed masking algorithm

background-subtraction.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 background = cv2.imread('Background_Color.jpg')
-frame = cv2.imread('StableOutput_Color1.jpg')
+frame = cv2.imread('StableOutput_Color2.jpg')
 
 diff = cv2.absdiff(frame, background)
 cv2.imwrite("diff.jpg", diff)
@@ -13,17 +13,18 @@ max_radius = 1000
 
 # gray_frame = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-# cv2.imwrite("OutputGray.jpg", gray_frame)
 
-lower_blue = np.array([20, 20, 20])
-upper_blue = np.array([230, 230, 230])
+lower_blue = np.array([10, 10, 10])
+upper_blue = np.array([255, 255, 255])
 
 gray_frame = cv2.inRange(diff, lower_blue, upper_blue)
-gray_frame = cv2.medianBlur(gray_frame, 11)
+gray_frame = cv2.medianBlur(gray_frame, 9)
+
+cv2.imwrite("OutputGray.jpg", gray_frame)
 
 cv2.imwrite("gray.jpg", gray_frame)
 
-balls = cv2.HoughCircles(gray_frame, cv2.cv.CV_HOUGH_GRADIENT, 5, minDist=25, param1=100, param2=100, minRadius=5, maxRadius=30)
+balls = cv2.HoughCircles(gray_frame, cv2.cv.CV_HOUGH_GRADIENT, 5, minDist=20, param1=100, param2=60, minRadius=17, maxRadius=30)
 
 detector = cv2.SimpleBlobDetector()
 
@@ -35,7 +36,7 @@ im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255),
 
 cv2.imwrite("blobballs.jpg", im_with_keypoints)
 
-print("Detected balls:")
+print("Detected balls:") 
 print(balls)
 balls = np.round(balls[0, :]).astype("int")
 