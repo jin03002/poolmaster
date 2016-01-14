22d0e91abc7eee6c441e60c9753dcfc4590f24dd
jin03002 <jin03002@users.noreply.github.com>
2016-01-14 16:43:03 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -87,69 +87,46 @@ std::vector<std::string> split(const std::string &s, char delim) {
     return elems;
 }
 
-int main(int argc, char * const argv[]) {
+void readBallFile(pair<int, int> ballOutput) {
   ifstream file("OutputBalls.csv");
-
-  pair<int, int> balls[16];
   string line;
+
   if (file.is_open()) {
     int i = 0;
-    while (getline(file, line) && i < sizeof(balls)/sizeof(*balls)) {
+    while (getline(file, line) && i < sizeof(ballOutput)/sizeof(*ballOutput)) {
       vector<string> v = split(line, ',');
-      balls[i] = make_pair(atoi(v[0].c_str()), atoi(v[1].c_str()));
+      ballOutput[i] = make_pair(atoi(v[0].c_str()), atoi(v[1].c_str()));
       i++;
     }
   }
+}
 
-  // Output version info
-  // cerr << getFastFizVersion() << endl;
-  // cerr << getRulesVersion() << endl;
-
-  // Start a log file
-  // LogWriter lw("example.log",GT_EIGHTBALL,&gn,"Example Agent");
-  // Ball::Type blist={Ball::CUE, Ball::ONE, Ball::TWO, Ball:: THREE, Ball::FOUR, Ball::FIVE,
-  //                   Ball::SIX, Ball::SEVEN, Ball::EIGHT, Ball::NINE, Ball::TEN, Ball::ELEVEN,
-  //                   Ball::TWELVE, Ball::THIRTEEN, Ball::FOURTEEN, Ball::FIFTEEN};
-
-
-  TableState* ts = new TableState();
-  // for (int i = 0; i < sizeof(balls)/; i++) {
-  //   ts.setBall(blist[i], Ball::STATIONARY, balls[i][0], balls[i][1]);
-  // }
-
-  // cout << balls[0].second << endl;
-
-  ts->setBall(Ball::CUE,      Ball::STATIONARY, balls[0].first, balls[0].second);
-  ts->setBall(Ball::ONE,      Ball::STATIONARY, balls[1].first, balls[1].second);
-  ts->setBall(Ball::TWO,      Ball::STATIONARY, balls[2].first, balls[2].second);
-  ts->setBall(Ball::THREE,    Ball::STATIONARY, balls[3].first, balls[3].second);
-  ts->setBall(Ball::FOUR,     Ball::STATIONARY, balls[4].first, balls[4].second);
-  ts->setBall(Ball::FIVE,     Ball::STATIONARY, balls[5].first, balls[5].second);
-  ts->setBall(Ball::SIX,      Ball::STATIONARY, balls[6].first, balls[6].second);
-  ts->setBall(Ball::SEVEN,    Ball::STATIONARY, balls[7].first, balls[7].second);
-  ts->setBall(Ball::EIGHT,    Ball::STATIONARY, balls[8].first, balls[8].second);
-  ts->setBall(Ball::NINE,     Ball::STATIONARY, balls[9].first, balls[9].second);
+void placeBalls(TableState ts, pair<int, int> balls) {
+  ts->setBall(Ball::CUE,      Ball::STATIONARY, balls[0].first,  balls[0].second);
+  ts->setBall(Ball::ONE,      Ball::STATIONARY, balls[1].first,  balls[1].second);
+  ts->setBall(Ball::TWO,      Ball::STATIONARY, balls[2].first,  balls[2].second);
+  ts->setBall(Ball::THREE,    Ball::STATIONARY, balls[3].first,  balls[3].second);
+  ts->setBall(Ball::FOUR,     Ball::STATIONARY, balls[4].first,  balls[4].second);
+  ts->setBall(Ball::FIVE,     Ball::STATIONARY, balls[5].first,  balls[5].second);
+  ts->setBall(Ball::SIX,      Ball::STATIONARY, balls[6].first,  balls[6].second);
+  ts->setBall(Ball::SEVEN,    Ball::STATIONARY, balls[7].first,  balls[7].second);
+  ts->setBall(Ball::EIGHT,    Ball::STATIONARY, balls[8].first,  balls[8].second);
+  ts->setBall(Ball::NINE,     Ball::STATIONARY, balls[9].first,  balls[9].second);
   ts->setBall(Ball::TEN,      Ball::STATIONARY, balls[10].first, balls[10].second);
   ts->setBall(Ball::ELEVEN,   Ball::STATIONARY, balls[11].first, balls[11].second);
   ts->setBall(Ball::TWELVE,   Ball::STATIONARY, balls[12].first, balls[12].second);
   ts->setBall(Ball::THIRTEEN, Ball::STATIONARY, balls[13].first, balls[13].second);
   ts->setBall(Ball::FOURTEEN, Ball::STATIONARY, balls[14].first, balls[14].second);
   ts->setBall(Ball::FIFTEEN,  Ball::STATIONARY, balls[15].first, balls[15].second);
+}
 
-  cout << ts->toString() << endl;
+void tryAllShots(TableState ts, ) {
 
-  try{
-      Shot* shot = (*ts).executeShot(ShotParams(0.1, 0.1, 25.3, 274.0, 2.0));
+}
 
-  // } catch (BadShotException e) {
-  } catch (char const* msg) {
-      cout << msg << endl;
-  }
-
-  cout << ts->toString() << endl;
-
+void writeTableFile(TableState ts) {
   ofstream outFile;
-  outFile.open ("out_state.csv");
+  outFile.open("out_state.csv");
   outFile << ts->getBall(Ball::CUE).getPos().x << "," << ts->getBall(Ball::CUE).getPos().y << "," << 0.028575 << endl;
   outFile << ts->getBall(Ball::ONE).getPos().x << "," << ts->getBall(Ball::ONE).getPos().y << "," << 0.028575 << endl;
   outFile << ts->getBall(Ball::TWO).getPos().x << "," << ts->getBall(Ball::TWO).getPos().y << "," << 0.028575 << endl;
@@ -168,26 +145,44 @@ int main(int argc, char * const argv[]) {
   outFile << ts->getBall(Ball::FIFTEEN).getPos().x << "," << ts->getBall(Ball::FIFTEEN).getPos().y << "," << 0.028575 << endl;
 
   outFile.close();
+}
 
-  // A typical break shot
-  // GameShot myShot = {ShotParams(0.0, 0.0, 25.0, 270.0, 5.0), // Shot parameters
-  //   0.48, 1.67705, // Cue position
-  //   Ball::UNKNOWN_ID,Table::UNKNOWN_POCKET, // No need to call ball/pocket
-  //   DEC_NO_DECISION, // No decision to make.
-  //   0.0}; // No time spent.
-  //
-  // cerr << "Shot result is: " << res << endl;
-  //
-  // if (res == SR_OK) { // If you kept your turn
-  //   srand ( time(NULL) );
-  //   GameShot myNextShot = {
-  //     ShotParams(0.0,0.0,25.0,(360.0*rand())/RAND_MAX,5.0), // random direction
-  //     0.0, 0.0, // cue position not needed
-  //     Ball::THREE,Table::SW, // call a ball and a pocket
-  //     DEC_NO_DECISION, // No decision to make.
-  //     0.0}; // No time spent.
-  //   res = st.executeShot(myNextShot); // Execute the shot on the table (no noise added).
+int main(int argc, char * const argv[]) {
+  // Output version info
+  // cerr << getFastFizVersion() << endl;
+  // cerr << getRulesVersion() << endl;
+
+  // Start a log file
+  // LogWriter lw("example.log",GT_EIGHTBALL,&gn,"Example Agent");
+  // Ball::Type blist={Ball::CUE, Ball::ONE, Ball::TWO, Ball:: THREE, Ball::FOUR, Ball::FIVE,
+  //                   Ball::SIX, Ball::SEVEN, Ball::EIGHT, Ball::NINE, Ball::TEN, Ball::ELEVEN,
+  //                   Ball::TWELVE, Ball::THIRTEEN, Ball::FOURTEEN, Ball::FIFTEEN};
+
+  pair<int, int> balls[16];
+  readBallFile(balls); // balls is the output array
+  TableState* ts = new TableState();
+  // for (int i = 0; i < sizeof(balls)/; i++) {
+  //   ts.setBall(blist[i], Ball::STATIONARY, balls[i][0], balls[i][1]);
   // }
+
+  // cout << balls[0].second << endl;
+
+  placeBalls(ts, balls);
+
+  cout << ts->toString() << endl;
+
+  try {
+    ShotParams params = ShotParams(0.1, 0.1, 25.3, 274.0, 2.0);
+    Shot* shot = (*ts).executeShot(params);
+  // } catch (BadShotException e) {
+  } catch (char const* msg) {
+    cout << msg << endl;
+  }
+
+  if (shot == SR_OK) {
+    cout << ts->toString() << endl;
+    writeTableFile(ts);
+  }
 
   return 0;
 }
